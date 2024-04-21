20d249fc4e90f9bc10b5d66ef7ad866fc041d7cb
Jason <devop.jas@gmail.com>
2024-04-21 09:28:17 +0800

    PYTHON

---
+++ welcome.py
@@ -0,0 +1,39 @@
+#PYTHON ASSIGNMENT OPERATORS 
+
+#addition 
+#substraction 
+#multiplication 
+#divition
+#modulus
+#Exponentiation
+#floor divistiomn 
+
+
+#PYTHON COMPARISON OPERATORS 
+
+#equal
+#not equal
+#greater than 
+#less than 
+#greater than or equal to 
+#less than or equal to
+
+#PYTHON LOGICAL OPERATORS
+
+
+#and
+#or
+#not
+
+#PYTHON INDENTITY OPERATORS
+
+#is
+#is not
+
+#PYTHON MEMBERSHIP OPERATORS
+
+#in
+#not in 
+
+#PYTHON BITWISE OPERATORS
+
