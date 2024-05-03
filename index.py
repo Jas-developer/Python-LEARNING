bf395303695f9b76d327f4b3b7b102392ceb9f2e
Jason <devop.jas@gmail.com>
2024-05-03 14:39:21 +0800

    Learning OOP

--- index.py
+++ index.py
@@ -40,6 +40,40 @@ def returnAllData():
     return data
 
 #extract the data from the function
-
 for x in returnAllData():
     print(x)
+    
+testName = ["Jason","John Doe","Manny"] #this is a list/array of s trings#
+#append method
+testName.append("Floyd")
+print(testName)
+
+for x in testName:
+    if(x == "Floyd"):
+        continue
+    print(x)
+    
+#clear method
+testName.clear()
+#printing the names
+
+
+#OOP In python
+#Object oriented programming
+#creating an object with  class in python
+
+
+class myFullName:
+    firstName = "Jason"
+    lastName =  "Evaristo"
+    dream = "Data Engineer"
+    
+p1 = myFullName();
+firstName = p1.firstName
+lastName = p1.lastName
+
+fullName = f"My fullName is {firstName} {lastName} and I want to be a {p1.dream}"
+#labeler
+print("Currently Learning OOP in python")
+print(fullName)
+
