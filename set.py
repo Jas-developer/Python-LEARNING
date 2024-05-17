ec67a35c4a65d0f2444b5b0fdc7e3dc48af24199
Jason <devop.jas@gmail.com>
2024-05-17 20:41:51 +0800

    learning sets

--- set.py
+++ set.py
@@ -1,19 +1,21 @@
 
 #set in python 
-utensils = {}
+utensils = {"fork","spoon","knife"} #this is a set 
+dishes = {"bowl","plate","cup", "knife"}
 
 
+#i would like to see what utensils have that dishes doesnt
+#print(utensils.difference(dishes))
+
+#I would like to see what they have in common 
+common = utensils.intersection(dishes)
+difference = utensils.difference(dishes)
+
+print(f"What they have in common is :{common}")
+print(f"Their difference is : {difference}")
 
 
-
-
-
-
-
-
-
-
-
+#a set is a collection that is unordered and un indexed
 
 
 
